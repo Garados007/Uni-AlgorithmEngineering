a2fe38d64bf2a951fd664ba970d7c2c8d89821c1
Max Brauer <ma.brauer@live.de>
2023-05-05 11:54:40 +0200

    Move EdgeWeightLowerRowParser into own file

--- src/Library/Parser/EdgeWeightParser.cs
+++ src/Library/Parser/EdgeWeightParser.cs
@@ -1,5 +1,3 @@
-using Library.EdgeWeights;
-
 namespace Library.Parser;
 
 public sealed class EdgeWeightParser : ParserBase<EdgeWeight>
@@ -24,40 +22,5 @@ public sealed class EdgeWeightParser : ParserBase<EdgeWeight>
             default:
                 throw new ParseException($"edge weight format {Specification.EdgeWeightFormat} not supported");
         }
-    }
-}
-
-public sealed class EdgeWeightLowerRowParser : ParserBase<EdgeWeight>
-{
-    public EdgeWeightLowerRowParser(int dimension)
-    {
-        if (dimension < 0)
-            throw new ParseException("Dimension has to be at least 0");
-        Dimension = dimension;
-    }
-
-    public int Dimension { get; }
-
-    public override EdgeWeight? Parse(ref State state)
-    {
-        var data = new EdgeWeightLowerRow(Dimension);
-        for (int from = 1; from < Dimension; ++from)
-        {
-            for (int to = 0; to < from; ++to)
-            {
-                var num = ReadInt32(ref state);
-                if (num is null)
-                {
-                    if (to == 0)
-                        return data;
-                    else throw new ParseException(ref state, $"node weight expected for ({from}, {to})");
-                }
-                data[from, to] = num.Value;
-                if (to + 1 < from && !SkipWhitespace(ref state))
-                    throw new ParseException(ref state, "whitespace expected");
-            }
-            SkipUntilEndOfLine(ref state);
-        }
-        return data;
     }
 }
